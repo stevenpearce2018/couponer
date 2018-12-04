b26103d36c00a124ff253fdb6826f992f705cb6e
Pearce <spearce5@LAMU02WX5G7HTD6.uhc.com>
2018-12-04 12:35:40 -0500

    Updated routing

--- client/src/components/CouponForm/couponform.js
+++ client/src/components/CouponForm/couponform.js
@@ -67,14 +67,14 @@ class CouponForm extends Component {
   }
   componentDidMount() {
     const loggedInKey = sessionStorage.getItem('UnlimitedCouponerKey');
-    // if (!loggedInKey) {
-    //   this.props.setMainHome()
-    //   return toast.error('You are not logged in!')
-    // }
-    // else if(loggedInKey.slice(-1) !== "b") {
-    //   this.props.setMainHome()
-    //   return toast.error('Only buiness owners can access this page!')
-    // }
+    if (!loggedInKey) {
+      this.props.setMainHome()
+      return toast.error('You are not logged in!')
+    }
+    else if(loggedInKey.slice(-1) !== "b") {
+      this.props.setMainHome()
+      return toast.error('Only buiness owners can access this page!')
+    }
     this.setState({loggedInKey:loggedInKey})
     const couponlatitude = sessionStorage.getItem('couponlatitude');
     const couponlongitude = sessionStorage.getItem('couponlongitude');
