20807f6e68f5bb003dff659dae9e91046ad5daef
Pearce <spearce5@LAMU02WX5G7HTD6.uhc.com>
2018-12-05 10:22:17 -0500

    Coupon maker discard UI update

--- client/src/couponsMaker.js
+++ client/src/couponsMaker.js
@@ -13,12 +13,55 @@ const longitude = sessionStorage.getItem('couponlongitude');
 const showCode = (code, showPopup, title) => showPopup(code, title);
 const validateCode = (_id, showPopup, title) => showPopup(_id, title);
 
-const getCoupons = async (_id, updateCouponsClaimed) => {
+// const getCoupons = async (_id, updateCouponsClaimed) => {
+//   const loggedInKey = sessionStorage.getItem('UnlimitedCouponerKey') ? sessionStorage.getItem('UnlimitedCouponerKey').replace('"', '').replace('"', '') : null;
+//   const email = sessionStorage.getItem('UnlimitedCouponerEmail') ? sessionStorage.getItem('UnlimitedCouponerEmail') : null;
+//   if (!loggedInKey || !email) {
+//     toast.error('You are not logged in!')
+//     // window.location.href = '/Login'
+//     window.history.pushState(null, '', decodeURIComponent(`/Login`));
+//   }
+//   else {
+//     const data = {
+//       _id: _id,
+//       loggedInKey: loggedInKey,
+//       email: email
+//     }
+//     const response = await postRequest(`/api/getCoupon`, data)
+//     const couponsCurrentlyClaimed = Number(sessionStorage.getItem('couponsCurrentlyClaimed')) + 1;
+//     sessionStorage.setItem('couponsCurrentlyClaimed', couponsCurrentlyClaimed )
+//     updateCouponsClaimed(1)
+//     if (response.response === "Coupon Claimed!") toast.success("Coupon Claimed!")
+//     else toast.error(response.response)
+//   }
+// }
+// const discardCoupons = async (_id, updateCouponsClaimed) => {
+//   const loggedInKey = sessionStorage.getItem('UnlimitedCouponerKey') ? sessionStorage.getItem('UnlimitedCouponerKey').replace('"', '').replace('"', '') : null;
+//   const email = sessionStorage.getItem('UnlimitedCouponerEmail') ? sessionStorage.getItem('UnlimitedCouponerEmail') : null;
+//   if (!loggedInKey || !email) {
+//     toast.error('You are not logged in!')
+//     window.history.pushState(null, '', decodeURIComponent(`/Login`));
+//   }
+//   else {
+//     const data = {
+//       _id: _id,
+//       loggedInKey: loggedInKey,
+//       email: email
+//     }
+//     const response = await postRequest(`/api/discardCoupon`, data)
+//     const couponsCurrentlyClaimed = Number(sessionStorage.getItem('couponsCurrentlyClaimed')) - 1;
+//     sessionStorage.setItem('couponsCurrentlyClaimed', couponsCurrentlyClaimed )
+//     updateCouponsClaimed(-1)
+//     if (response.response === "Coupon Removed!") toast.success("Coupon Removed!")
+//     else toast.error(response.response)
+//   }
+// }
+
+const getOrDiscardCoupons = async (_id, updateCouponsClaimed, flag) => {
   const loggedInKey = sessionStorage.getItem('UnlimitedCouponerKey') ? sessionStorage.getItem('UnlimitedCouponerKey').replace('"', '').replace('"', '') : null;
   const email = sessionStorage.getItem('UnlimitedCouponerEmail') ? sessionStorage.getItem('UnlimitedCouponerEmail') : null;
   if (!loggedInKey || !email) {
     toast.error('You are not logged in!')
-    // window.location.href = '/Login'
     window.history.pushState(null, '', decodeURIComponent(`/Login`));
   }
   else {
@@ -28,10 +71,12 @@ const getCoupons = async (_id, updateCouponsClaimed) => {
       email: email
     }
     const response = await postRequest(`/api/getCoupon`, data)
-    const couponsCurrentlyClaimed = Number(sessionStorage.getItem('couponsCurrentlyClaimed')) + 1;
-    sessionStorage.setItem('couponsCurrentlyClaimed', couponsCurrentlyClaimed )
-    updateCouponsClaimed(1)
-    if (response.response === "Coupon Claimed!") toast.success("Coupon Claimed!")
+    if (response.response === "Coupon Claimed!" || response.response === "Coupon Removed!") {
+      toast.success(response.response)
+      const couponsCurrentlyClaimed = flag === "discard" ? Number(sessionStorage.getItem('couponsCurrentlyClaimed')) - 1 : Number(sessionStorage.getItem('couponsCurrentlyClaimed')) + 1; 
+      sessionStorage.setItem('couponsCurrentlyClaimed', couponsCurrentlyClaimed )
+      flag === "discard" ? updateCouponsClaimed(-1) : updateCouponsClaimed(1)
+    }
     else toast.error(response.response)
   }
 }
@@ -73,11 +118,16 @@ const CouponsMaker = (props, updateCouponsClaimed, showPopup) => {
         <hr/>
         <br/>
         {
+          (window.location.href.substring(window.location.href.lastIndexOf('/')+1, window.location.href.length).toLowerCase() === "mycoupons" && sessionStorage.getItem('UnlimitedCouponerKey').substr(-1) === "c") ?
+          <button className="getCoupon red" onClick={ () => getOrDiscardCoupons(coupons._id, updateCouponsClaimed, "discard")}> Discard Coupon </button> :
+          <div></div>
+        }
+        {
           (window.location.href.substring(window.location.href.lastIndexOf('/')+1, window.location.href.length).toLowerCase() === "mycoupons" && sessionStorage.getItem('UnlimitedCouponerKey').substr(-1) === "b") ?
             <button className="getCoupon" onClick={ () => validateCode(coupons._id, showPopup, coupons.title)}> Validate Customer Codes </button> :
           (window.location.href.substring(window.location.href.lastIndexOf('/')+1, window.location.href.length).toLowerCase() === "mycoupons" && sessionStorage.getItem('UnlimitedCouponerKey').substr(-1) === "c") ? 
             <button className="getCoupon" onClick={ () => showCode(coupons.couponCodes[0], showPopup, coupons.title)}> Show Your Coupon Code </button> :
-            <button className="getCoupon" onClick={ () => getCoupons(coupons._id, updateCouponsClaimed)}> Get Coupon </button>
+            <button className="getCoupon" onClick={ () => getOrDiscardCoupons(coupons._id, updateCouponsClaimed, "get")}> Get Coupon </button>
         }
       </div>
       <br/>
