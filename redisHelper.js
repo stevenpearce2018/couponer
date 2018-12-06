a98925880568e1a0f09cc812ba282d16494eb96c
Pearce <spearce5@LAMU02WX5G7HTD6.uhc.com>
2018-12-06 10:04:18 -0500

    Simplified redis helper

--- redisHelper.js
+++ redisHelper.js
@@ -1,21 +1,17 @@
 const redis = require('redis')
 require('dotenv').config()
 const url = require('url');
-const redisURL = url.parse(process.env.REDISCLOUD_URL);
-const client = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-client.auth(process.env.REDISPASS);
+const redisURL = process.env.REDISCLOUD_URL ? url.parse(process.env.REDISCLOUD_URL) : console.log("No redis url found")
+const client = redisURL ? redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true}) : redis.createClient().then(console.log("No redis url found. Connecting to local redis server instead."))
+process.env.REDISPASS ? client.auth(process.env.REDISPASS) : console.log("No redis password found.")
 
 client.on('connect', () => console.log('Connected to redis'))
 
 // Convert object to string if value is an object
 // if a time to live is defined then give the data a time to expire
 const set = (key, value, ttl) => {
-    let valueToInsert;
-    if (typeof value == 'string') valueToInsert = value;
-    else valueToInsert = JSON.stringify(value);
-    
-    if (!ttl) client.set(key, valueToInsert)
-    else client.set(key, valueToInsert, 'EX', ttl)    
+    const valueToInsert = typeof value == 'string' ? valueToInsert = value : valueToInsert = JSON.stringify(value);
+    !ttl ? client.set(key, valueToInsert) : client.set(key, valueToInsert, 'EX', ttl);
 }
 const get = (key, callback) => {
     client.get(key, (error, result) => {
