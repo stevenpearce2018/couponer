6283371d15addac98ff20a0a2999e62583d04a0e
Pearce <spearce5@LAMU02WX5G7HTD6.uhc.com>
2019-01-03 14:07:56 -0500

    added https middleware

--- server.js
+++ server.js
@@ -31,6 +31,15 @@ const moment = require("moment");
 const checkPasswordStrength = require('./lib/checkPasswordStrength');
 const favicon = require('serve-favicon');
 
+const requireHTTPS = (req, res, next) => {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+app.use(requireHTTPS);
 app.use(favicon(__dirname + '/client/public/favicon.ico'));
 app.use(express.static('dist'));
 app.use(express.static(path.join(__dirname, "client", "build")))
