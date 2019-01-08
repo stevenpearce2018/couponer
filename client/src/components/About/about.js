e2e78668eb8da13a3a806fe4e64254e252043170
Pearce <spearce5@LAMU02WX5G7HTD6.uhc.com>
2019-01-08 11:14:07 -0500

    Site Performance Optimizations

--- client/src/components/About/about.js
+++ client/src/components/About/about.js
@@ -11,6 +11,9 @@ const About = () => {
         <AboutElement header={"Why choose us?"} text={"If you are a consumer you can find new and interesting events or foods that you may have never knew existed otherwise. As a consumer you can use UnlimitedCouponer to save money on activities that you would have done regardless, it pays for itself! If you are a Business Owner you can use UnlimitedCouponer to advertise your business and make money at the same time, other coupon websites take a large percentage of each sale. We believe that is not only anti-entrepreneur, but these aggressive margins can often times drive away up and coming small businesses."}/>
         <br/>
         <br/>
+        <AboutElement header={"How can we find you these deals?"} text={"Simple, businesses want to promote their products and they want to be able to cheaply do so. Being able to promote your business by offering discounts is an excellent model for building long term customers and UnlimitedCouponer's lack of overhead allows us to charge basically nothing to these mom and pop shops. This allows small and local businesses to have worry free promotions."}/>
+        <br/>
+        <br/>
         <AboutElement header={"Contact us"} text={"Found a bug, have a general question, want to make a sugguestion? Email me at UnlimitedCouponer@gmail.com."}/>
         </section>
         <br/>
